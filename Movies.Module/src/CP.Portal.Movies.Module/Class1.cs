a0be44a9478b8c5030895fab559e25aa01b24f2a
Joakin <jalberto2891@gmail.com>
2026-03-25 13:32:20 -0300

    Implement movie endpoints and update API routes

--- Movies.Module/src/CP.Portal.Movies.Module/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CP.Portal.Movies.Module;
-
-public class Class1
-{
-
-}
---
+++ Movies.Module/src/CP.Portal.Movies.Module/MovieEndpoints.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace CP.Portal.Movies.Module;
+
+public static class MovieEndpoints
+{
+    public static void MapMoviesEndpoints(this WebApplication app) 
+    {
+        app.MapGet("/test", () =>
+        {
+            return "Hello World Joakin!";
+        });
+    }
+}
